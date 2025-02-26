ccd389f554c2952daa6209f6dbc43c9855b43040
Niek Veenstra <niekveenstra14@gmail.com>
2025-02-26 12:16:24 +0100

    Added profile file to config.

---
+++ .profile.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+#REGION SHELL-UTIL
+function reload() {
+  source "$HOME/.zshrc"
+}
+
+function profile() {
+  nvim "$HOME/.profile.sh"
+}
+
+function c() {
+  clear
+}
+
+function iptables() {
+  sudo iptables $@
+}
+#END REGION SHELL-UTIL
+
+# REGION GIT.
+function branch() {
+  git branch $@
+}
+
+function push() {
+  git push $@
+}
+
+function commit() {
+  git commit $@
+}
+
+function add() {
+  git add $@
+}
+
+# END REGION GIT.
+
+# REGION DOCKER.
+function dps() {
+  docker ps
+}
+
+function dkill() {
+  docker container stop $@
+}
+
+function dcu() {
+  docker compose up -d
+}
+
+function dis() {
+  docker container inspect $@
+}
+# END REGION DOCKER
